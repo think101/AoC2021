cc11da8a673a41120ade2b8bde550b3fc2cf2813
think101 <taohuayuan2000@hotmail.com>
2021-12-17 02:45:06 -0800

    day9 2nd final

--- src/main/java/com/aoc2021/day9.java
+++ src/main/java/com/aoc2021/day9.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class day9 {
@@ -12,8 +13,10 @@ public class day9 {
         int[][] input = getHeightMap();
 
         //System.out.println(Arrays.deepToString(input));
+        List<Integer> basins = calcBasins(input);
 
-        System.out.println(calcRisk(input));
+        basins.sort(Collections.reverseOrder());
+        System.out.println(basins.get(0) * basins.get(1) * basins.get(2));
     }
 
     private static int calcRisk(int[][] input) {
@@ -31,6 +34,48 @@ public class day9 {
             }
         }
         return risk;
+    }
+
+
+    // return all basin sizes
+    private static List<Integer> calcBasins(int[][] input) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input[i].length; j++) {
+                if((i-1 >=0 && input[i-1][j] <= input[i][j])
+                        || (i+1 < input.length && input[i+1][j] <= input[i][j])
+                        || (j-1 >=0 && input[i][j-1] <= input[i][j])
+                        || (j+1 < input[i].length && input[i][j+1] <= input[i][j])) {
+                    continue;
+                }
+
+                result.add(calcBasinSize(input, i, j));
+            }
+        }
+        return result;
+    }
+
+    private static int calcBasinSize(int[][] input, int i, int j) {
+        int size = 0;
+        int[][] visited = new int[input.length][input[0].length];
+        return calcBasinSize(input, i, j, visited);
+    }
+
+    private static int calcBasinSize(int[][] input, int i, int j, int[][] visited) {
+        if(i < 0 || i >= input.length || j < 0 || j >= input[i].length || visited[i][j] == 1 || input[i][j] == 9) {
+            return 0;
+        }
+
+        int size = 0;
+
+        visited[i][j] = 1;
+        size++;
+        size += calcBasinSize(input, i-1, j, visited);
+        size += calcBasinSize(input, i+1, j, visited);
+        size += calcBasinSize(input, i, j-1, visited);
+        size += calcBasinSize(input, i, j+1, visited);
+
+        return size;
     }
 
     private static int[][] getHeightMap() {
