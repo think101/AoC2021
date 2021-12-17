bbb5f9a1f8bf50d53d59c7b24dd720d0a37372f4
think101 <taohuayuan2000@hotmail.com>
2021-12-17 00:10:28 -0800

    day6 2nd final

--- src/main/java/com/aoc2021/day6.java
+++ src/main/java/com/aoc2021/day6.java
@@ -1,52 +1,50 @@
 package main.java.com.aoc2021;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.stream.LongStream;
 
 public class day6 {
 
     public static void main(String[] args) {
 
-        int[] input = new int[]{3,4,3,1,2};
-//        int[] input = new int[]{2,1,1,1,1,1,1,5,1,1,1,1,5,1,1,3,5,1,1,3,1,1,3,1,4,4,4,5,1,1,1,3,1,3,1,1,2,
-//                2,1,1,1,5,1,1,1,5,2,5,1,1,2,1,3,3,5,1,1,4,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,4,1,5,1,2,1,1,1,1,
-//                5,1,1,1,1,1,5,1,1,1,4,5,1,1,3,4,1,1,1,3,5,1,1,1,2,1,1,4,1,4,1,2,1,1,2,1,5,1,1,1,5,1,2,2,1,
-//                1,1,5,1,2,3,1,1,1,5,3,2,1,1,3,1,1,3,1,3,1,1,1,5,1,1,1,1,1,1,1,3,1,1,1,1,3,1,1,4,1,1,3,2,1,
-//                2,1,1,2,2,1,2,1,1,1,4,1,2,4,1,1,4,4,1,1,1,1,1,4,1,1,1,2,1,1,2,1,5,1,1,1,1,1,5,1,3,1,1,2,3,
-//                4,4,1,1,1,3,2,4,4,1,1,3,5,1,1,1,1,4,1,1,1,1,1,5,3,1,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-//                1,1,5,1,1,1,1,1,1,1,1,5,1,4,4,1,1,1,1,1,1,1,1,3,1,3,1,4,1,1,2,2,2,1,1,2,1,1};
+        //int[] input = new int[]{3,4,3,1,2};
+        int[] input = new int[]{2,1,1,1,1,1,1,5,1,1,1,1,5,1,1,3,5,1,1,3,1,1,3,1,4,4,4,5,1,1,1,3,1,3,1,1,2,
+                2,1,1,1,5,1,1,1,5,2,5,1,1,2,1,3,3,5,1,1,4,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,4,1,5,1,2,1,1,1,1,
+                5,1,1,1,1,1,5,1,1,1,4,5,1,1,3,4,1,1,1,3,5,1,1,1,2,1,1,4,1,4,1,2,1,1,2,1,5,1,1,1,5,1,2,2,1,
+                1,1,5,1,2,3,1,1,1,5,3,2,1,1,3,1,1,3,1,3,1,1,1,5,1,1,1,1,1,1,1,3,1,1,1,1,3,1,1,4,1,1,3,2,1,
+                2,1,1,2,2,1,2,1,1,1,4,1,2,4,1,1,4,4,1,1,1,1,1,4,1,1,1,2,1,1,2,1,5,1,1,1,1,1,5,1,3,1,1,2,3,
+                4,4,1,1,1,3,2,4,4,1,1,3,5,1,1,1,1,4,1,1,1,1,1,5,3,1,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                1,1,5,1,1,1,1,1,1,1,1,5,1,4,4,1,1,1,1,1,1,1,1,3,1,3,1,4,1,1,2,2,2,1,1,2,1,1};
 
         System.out.println(calcLanternFish(input, 256));
 
     }
 
-    public static int calcLanternFish(int[] input, int days){
-        List<Integer> list = new ArrayList<>();
+    public static long calcLanternFish(int[] input, int days){
+        long[] stats = new long[9];
+
         for (int k : input) {
-            list.add(k);
+            stats[k]++;
         }
 
         for(int i = 0; i < days; i++){
-            int zeroCount = 0;
-            for(int j = 0; j < list.size(); j++){
-                int t = list.get(j);
+            long zeroCount = stats[0];
 
-                if(t == 0){
-                    zeroCount++;
-                    list.set(j, 6);
-                }
-                else {
-                    list.set(j, t - 1);
-                }
+            for(int j = 1; j < stats.length; j++){
+                stats[j-1] = stats[j];
             }
 
-            for (int j = 0; j < zeroCount; j++) {
-                list.add(8);
-            }
+            stats[stats.length-1] = zeroCount;
+            stats[6] = stats[6] + zeroCount;
 
-            //System.out.println(list);
+            //System.out.println(Arrays.toString(stats));
         }
 
-        return list.size();
+        return LongStream.of(stats).sum();
     }
 }
+
+
+
+// hint 1: To solve this puzzle, you only need a 9-element array.
+// hint 2: use long instead of int for the array elements, otherwise int overflows cause wrong result.
